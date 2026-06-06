0b132dc439206f16dfa5b3473056657905d951db
samandarozodbekovich <nosirovsamandar110@gmail.com>
2026-06-06 15:58:35 +0500

    superadmin23

---
+++ create_superuser.py
@@ -0,0 +1,22 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'find_work.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = 'admin'
+password = 'admin1234'
+
+if User.objects.filter(username=username).exists():
+    u = User.objects.get(username=username)
+    u.is_staff = True
+    u.is_superuser = True
+    u.set_password(password)
+    u.save()
+    print("Superuser yangilandi")
+else:
+    User.objects.create_superuser(username, 'admin@mail.com', password)
+    print("Superuser yaratildi")
