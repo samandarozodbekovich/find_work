57b367269019cd893dbb240815beea1b52d4149e
samandarozodbekovich <nosirovsamandar110@gmail.com>
2026-06-06 15:42:30 +0500

    superadmin

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser \
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL || true
