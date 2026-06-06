d78f1183eb16149773f6811c6458e509a9ec5f29
samandarozodbekovich <nosirovsamandar110@gmail.com>
2026-06-06 14:33:40 +0500

    fix settings

--- find_work/settings.py
+++ find_work/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
